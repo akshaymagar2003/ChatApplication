69fa25768ef8a2cc5fa6098745a0109a8e0f40bd
Akshay@2003 <akshay88magar@gmail.com>
2023-06-07 21:57:43 +0530

    Login Activity functioning implemented

--- app/src/main/java/com/example/chatapplication/Login.kt
+++ app/src/main/java/com/example/chatapplication/Login.kt
@@ -3,20 +3,55 @@ package com.example.chatapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.chatapplication.databinding.ActivityLoginBinding
+import com.example.chatapplication.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
-    lateinit var  binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-        supportActionBar?.hide()
-   binding.Register.setOnClickListener {
-       val intent= Intent(this,Register::class.java)
-       startActivity(intent)
-   }
+        binding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth= FirebaseAuth.getInstance()
+        binding.Register.setOnClickListener {
+            var intent =Intent(this,Register::class.java)
+            startActivity(intent)
+            finish()
+        }
 
+
+        binding.Login.setOnClickListener {
+            if(checking()){
+                val email=binding.Email.text.toString()
+                val password= binding.Password.text.toString()
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            var intent =Intent(this,MainActivity::class.java)
+                            intent.putExtra("email",email)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Toast.makeText(this, "Wrong Details", Toast.LENGTH_LONG).show()
+                        }
+                    }
+            }
+            else{
+                Toast.makeText(this,"Enter the Details", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+    private fun checking():Boolean
+    {
+        if(binding.Email.text.toString().trim{it<=' '}.isNotEmpty()
+            && binding.Password.text.toString().trim{it<=' '}.isNotEmpty())
+        {
+            return true
+        }
+        return false
     }
 }
