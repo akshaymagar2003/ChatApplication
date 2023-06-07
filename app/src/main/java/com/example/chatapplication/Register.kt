86a575720fe85a346414eb743f5cacd1ba198823
Akshay@2003 <akshay88magar@gmail.com>
2023-06-07 21:59:14 +0530

    Register Activity functioning implemented

--- app/src/main/java/com/example/chatapplication/Register.kt
+++ app/src/main/java/com/example/chatapplication/Register.kt
@@ -1,12 +1,84 @@
 package com.example.chatapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.example.chatapplication.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Register : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    lateinit var binding: ActivityRegisterBinding
+    private lateinit var db: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding= ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth= FirebaseAuth.getInstance()
+        db= FirebaseFirestore.getInstance()
+        binding.Continue.setOnClickListener {
+            if(checking())
+            {
+                var email=binding.EmailRegister.text.toString()
+                var password= binding.PasswordRegister.text.toString()
+                var name=binding.Name.text.toString()
+                var phone=binding.Phone.text.toString()
+                val user= hashMapOf(
+                    "Name" to name,
+                    "Phone" to phone,
+                    "email" to email
+                )
+                val Users=db.collection("USERS")
+                val query =Users.whereEqualTo("email",email).get()
+                    .addOnSuccessListener {
+                            tasks->
+                        if(tasks.isEmpty)
+                        {
+                            auth.createUserWithEmailAndPassword(email,password)
+                                .addOnCompleteListener(this){
+                                        task->
+                                    if(task.isSuccessful)
+                                    {
+                                        Users.document(email).set(user)
+                                        val intent= Intent(this,MainActivity::class.java)
+                                        intent.putExtra("email",email)
 
+                                        startActivity(intent)
+                                        finish()
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(this,"Authentication Failed", Toast.LENGTH_LONG).show()
+                                    }
+                                }
+                        }
+                        else
+                        {
+                            Toast.makeText(this,"User Already Registered", Toast.LENGTH_LONG).show()
+                            val intent= Intent(this,MainActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
+            }
+            else{
+                Toast.makeText(this,"Enter the Details", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
+    private fun checking():Boolean{
+        if(binding.Name.text.toString().trim{it<=' '}.isNotEmpty()
+            && binding.Phone.text.toString().trim{it<=' '}.isNotEmpty()
+            && binding.EmailRegister.text.toString().trim{it<=' '}.isNotEmpty()
+            && binding.PasswordRegister.text.toString().trim{it<=' '}.isNotEmpty()
+        )
+        {
+            return true
+        }
+        return false
     }
 }
