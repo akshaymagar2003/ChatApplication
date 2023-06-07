1e0687ec7b5f979087f1e712cc250d6e584546df
Akshay@2003 <akshay88magar@gmail.com>
2023-06-07 08:27:25 +0530

    Login UI Binding done

--- app/src/main/java/com/example/chatapplication/Login.kt
+++ app/src/main/java/com/example/chatapplication/Login.kt
@@ -1,11 +1,22 @@
 package com.example.chatapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chatapplication.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+    lateinit var  binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        supportActionBar?.hide()
+   binding.Register.setOnClickListener {
+       val intent= Intent(this,Register::class.java)
+       startActivity(intent)
+   }
+
     }
 }
