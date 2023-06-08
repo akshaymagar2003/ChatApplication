fddcc78513a7e8c5027b502540e77a995130dd81
Akshay@2003 <akshay88magar@gmail.com>
2023-06-08 17:21:59 +0530

    chatActivity implemented

--- app/src/main/java/com/example/chatapplication/ChatActivity.kt
+++ app/src/main/java/com/example/chatapplication/ChatActivity.kt
@@ -1,50 +1,66 @@
 package com.example.chatapplication
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.chatapplication.databinding.ActivityMainBinding
-import com.google.firebase.firestore.FirebaseFirestore
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.chatapplication.databinding.ActivityChatBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class ChatActivity : AppCompatActivity() {
-    private lateinit var db: FirebaseFirestore
-    lateinit var binding: ActivityMainBinding
-
+   private lateinit var binding:ActivityChatBinding
+   private lateinit var messageAdapter: MessageAdapter
+   private lateinit var messageList: ArrayList<Message>
+   private  lateinit var  mdb:DatabaseReference
+   var receiverRoom:String?=null
+    var senderRoom:String?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding=ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val sharedPref=this?.getPreferences(Context.MODE_PRIVATE)?:return
-        val isLogin=sharedPref.getString("Email","1")
 
-        if(isLogin=="1")
-        {
-            var email=intent.getStringExtra("email")
-            if(email!=null)
-            {
-                setText(email)
-                with(sharedPref.edit())
-                {
-                    putString("Email",email)
-                    apply()
+        val name =intent.getStringExtra("name")
+        val receiverUid = intent.getStringExtra("uid")
+        val senderUid= FirebaseAuth.getInstance().currentUser?.uid
+        mdb=FirebaseDatabase.getInstance().getReference()
+        senderRoom=receiverUid+senderUid
+        receiverRoom=senderUid+receiverUid
+        supportActionBar?.title=name
+        messageList=ArrayList()
+        messageAdapter=MessageAdapter(this,messageList)
+
+        binding.chatRecyclerView.layoutManager=LinearLayoutManager(this)
+       binding.chatRecyclerView.adapter=messageAdapter
+        mdb.child("chats").child(senderRoom!!).child("messages")
+            .addValueEventListener(object:ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    messageList.clear()
+                    for(postSnapshot in snapshot.children){
+                        val message =postSnapshot.getValue(Message::class.java)
+                       messageList.add(message!!)
+                    }
+                    messageAdapter.notifyDataSetChanged()
                 }
-            }
-            else{
-                var intent = Intent(this,Login::class.java)
-                startActivity(intent)
-                finish()
-            }
-        }
-        else
-        {
-            setText(isLogin)
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
+
+
+        binding.sentButton.setOnClickListener{
+            val message=binding.messageBox.text.toString()
+            val messageObject=Message(message,senderUid)
+
+            mdb.child("chats").child(senderRoom!!).child("messages").push()
+                .setValue(messageObject).addOnSuccessListener {
+                    mdb.child("chats").child(receiverRoom!!).child("messages").push().setValue(messageObject)
+                }
+            binding.messageBox.setText("")
         }
 
-    }
-
-    private fun setText(email:String?)
-    {
 
 
     }
