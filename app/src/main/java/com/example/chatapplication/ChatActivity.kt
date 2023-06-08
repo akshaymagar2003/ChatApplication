8cab4c846b6fd1f9039486da8a272a89f5309dc7
Akshay@2003 <akshay88magar@gmail.com>
2023-06-09 00:17:30 +0530

    ChatActivity updated for group chat

--- app/src/main/java/com/example/chatapplication/ChatActivity.kt
+++ app/src/main/java/com/example/chatapplication/ChatActivity.kt
@@ -1,67 +1,143 @@
 package com.example.chatapplication
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chatapplication.databinding.ActivityChatBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
+
 class ChatActivity : AppCompatActivity() {
-   private lateinit var binding:ActivityChatBinding
-   private lateinit var messageAdapter: MessageAdapter
-   private lateinit var messageList: ArrayList<Message>
-   private  lateinit var  mdb:DatabaseReference
-   var receiverRoom:String?=null
-    var senderRoom:String?=null
+    private lateinit var binding: ActivityChatBinding
+    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var database: DatabaseReference
+    private lateinit var groupId: String
+    private var isGroupChat = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityChatBinding.inflate(layoutInflater)
+        binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val name =intent.getStringExtra("name")
-        val receiverUid = intent.getStringExtra("uid")
-        val senderUid= FirebaseAuth.getInstance().currentUser?.uid
-        mdb=FirebaseDatabase.getInstance().getReference()
-        senderRoom=receiverUid+senderUid
-        receiverRoom=senderUid+receiverUid
-        supportActionBar?.title=name
-        messageList=ArrayList()
-        messageAdapter=MessageAdapter(this,messageList)
+        // Retrieve intent extras
+        val intent = intent
+        val name = intent.getStringExtra("name")
+        val uid = intent.getStringExtra("uid")
+        groupId = intent.getStringExtra("groupId") ?: ""
 
-        binding.chatRecyclerView.layoutManager=LinearLayoutManager(this)
-       binding.chatRecyclerView.adapter=messageAdapter
-        mdb.child("chats").child(senderRoom!!).child("messages")
-            .addValueEventListener(object:ValueEventListener{
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    messageList.clear()
-                    for(postSnapshot in snapshot.children){
-                        val message =postSnapshot.getValue(Message::class.java)
-                       messageList.add(message!!)
+        // Check if it's a group chat
+        isGroupChat = !groupId.isEmpty()
+
+        // Set the title based on the chat type
+        supportActionBar?.title = if (isGroupChat) "Group Chat" else name
+
+        // Initialize Firebase database reference
+        database = FirebaseDatabase.getInstance().reference
+        // Set up RecyclerView
+        binding.chatRecyclerView.layoutManager = LinearLayoutManager(this)
+        messageAdapter = MessageAdapter(this, ArrayList())
+        binding.chatRecyclerView.adapter = messageAdapter
+
+        // Load messages based on chat type
+        if (isGroupChat) {
+            loadGroupMessages()
+        } else {
+            if (uid != null) {
+                loadIndividualMessages(uid)
+            }
+        }
+
+        // Send button click listener
+        binding.sentButton.setOnClickListener {
+            if (uid != null) {
+                sendMessage(uid)
+            }
+        }
+    }
+
+    private fun loadIndividualMessages(uid: String) {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val senderId = currentUser?.uid
+        val receiverId = uid
+
+        val chatRef = database.child("Chats")
+
+        if (senderId != null) {
+            chatRef.child(senderId).child(receiverId).addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val messageList = ArrayList<Message>()
+                    for (snapshot in dataSnapshot.children) {
+                        val message = snapshot.getValue(Message::class.java)
+                        message?.let { messageList.add(it) }
                     }
+                    messageAdapter.updateMessageList(messageList)
                     messageAdapter.notifyDataSetChanged()
                 }
 
-                override fun onCancelled(error: DatabaseError) {
-
+                override fun onCancelled(databaseError: DatabaseError) {
+                    Log.d("ChatActivity", "Error: ${databaseError.message}")
                 }
-
             })
-
-
-        binding.sentButton.setOnClickListener{
-            val message=binding.messageBox.text.toString()
-            val messageObject=Message(message,senderUid)
-
-            mdb.child("chats").child(senderRoom!!).child("messages").push()
-                .setValue(messageObject).addOnSuccessListener {
-                    mdb.child("chats").child(receiverRoom!!).child("messages").push().setValue(messageObject)
-                }
-            binding.messageBox.setText("")
         }
 
+        if (senderId != null) {
+            chatRef.child(receiverId).child(senderId).addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val messageList = ArrayList<Message>()
+                    for (snapshot in dataSnapshot.children) {
+                        val message = snapshot.getValue(Message::class.java)
+                        message?.let { messageList.add(it) }
+                    }
+                    messageAdapter.updateMessageList(messageList)
+                    messageAdapter.notifyDataSetChanged()
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    Log.d("ChatActivity", "Error: ${databaseError.message}")
+                }
+            })
+        }
+    }
 
 
+
+
+    private fun loadGroupMessages() {
+        Log.d("iamdon","group")
+        database.child("Groups").child(groupId).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val messageList = ArrayList<Message>()
+                for (snapshot in dataSnapshot.children) {
+                    val message = snapshot.getValue(Message::class.java)
+                    message?.let { messageList.add(it) }
+                }
+                messageAdapter.updateMessageList(messageList)
+                messageAdapter.notifyDataSetChanged()
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d("ChatActivity", "Error: ${databaseError.message}")
+            }
+        })
+    }
+
+    private fun sendMessage(uid: String) {
+        val messageText = binding.messageBox.text.toString().trim()
+
+        if (messageText.isNotEmpty()) {
+            val currentUser = FirebaseAuth.getInstance().currentUser
+            val senderId = currentUser?.uid
+            val receiverId = uid
+
+            val message = Message(messageText, senderId, receiverId, groupId)
+
+            // Save message to database
+            val messageId = database.child("Chats").child(senderId ?: "").child(receiverId).push().key
+            database.child("Chats").child(senderId ?: "").child(receiverId).child(messageId ?: "").setValue(message)
+
+            // Clear input field
+            binding.messageBox.text.clear()
+        }
     }
 }
