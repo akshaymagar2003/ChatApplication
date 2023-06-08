17ec5cc9d2d9e32d8dc7f420562a00742162fb41
Akshay@2003 <akshay88magar@gmail.com>
2023-06-09 00:16:25 +0530

    MessageAdapter updated for group chat

--- app/src/main/java/com/example/chatapplication/MessageAdapter.kt
+++ app/src/main/java/com/example/chatapplication/MessageAdapter.kt
@@ -1,61 +1,84 @@
 package com.example.chatapplication
-
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.google.firebase.auth.FirebaseAuth
 
-class MessageAdapter(val context:Context,val messageList: ArrayList<Message>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private val Item_Receive=1;
-    private val Item_Sent=2
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+class MessageAdapter(val context: Context, private var messageList: ArrayList<Message>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-             if(viewType==1){
-                   val view:View=LayoutInflater.from(context).inflate(R.layout.receive,parent,false)
-                    return ReceiveViewHolder(view)
-             }else{
-                 val view:View=LayoutInflater.from(context).inflate(R.layout.sent,parent,false)
-                 return SentViewHolder(view)
-             }
+    private val Item_Sent = 1
+    private val Item_Received = 2
+    private val Item_Group = 3
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(context)
+
+        return when (viewType) {
+            Item_Sent -> {
+                val view = inflater.inflate(R.layout.sent, parent, false)
+                SentViewHolder(view)
+            }
+            Item_Received -> {
+                val view = inflater.inflate(R.layout.receive, parent, false)
+                ReceivedViewHolder(view)
+            }
+            Item_Group -> {
+                val view = inflater.inflate(R.layout.group_layout, parent, false)
+                GroupViewHolder(view)
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val currentMessage = messageList[position]
+
+        when (holder.itemViewType) {
+            Item_Sent -> {
+                val sentViewHolder = holder as SentViewHolder
+                sentViewHolder.sentMessage.text = currentMessage.message
+            }
+            Item_Received -> {
+                val receivedViewHolder = holder as ReceivedViewHolder
+                receivedViewHolder.receivedMessage.text = currentMessage.message
+            }
+            Item_Group -> {
+                val groupViewHolder = holder as GroupViewHolder
+                groupViewHolder.groupSenderName.text = currentMessage.senderName
+                groupViewHolder.groupMessage.text = currentMessage.message
+            }
+        }
     }
 
     override fun getItemViewType(position: Int): Int {
-        val currentMessage=messageList[position]
-        if (FirebaseAuth.getInstance().currentUser?.uid.equals(currentMessage.senderId)){
-            return Item_Sent
-        }else{
-            return Item_Receive
+        val currentMessage = messageList[position]
+        return when {
+            FirebaseAuth.getInstance().currentUser?.uid == currentMessage.senderId -> Item_Sent
+            currentMessage.groupId != null -> Item_Group
+            else -> Item_Received
         }
-
     }
 
     override fun getItemCount(): Int {
-return messageList.size
+        return messageList.size
+    }
+    fun updateMessageList(messages: ArrayList<Message>) {
+        messageList = messages
+    }
+    inner class SentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val sentMessage: TextView = itemView.findViewById(R.id.txt_sent_message)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val currentMessage=messageList[position]
-  if(holder.javaClass==SentViewHolder::class.java){
-      val viewHolder=holder as SentViewHolder
-
-      viewHolder.sentMessage.text=currentMessage.message
-  }else{
-      val viewHolder=holder as ReceiveViewHolder
-      viewHolder.receiveMessage.text=currentMessage.message
-  }
-    }
-    class SentViewHolder(itemView: View):ViewHolder(itemView){
-    val sentMessage=itemView.findViewById<TextView>(R.id.txt_sent_message)
-
-    }
-    class ReceiveViewHolder(itemView: View):ViewHolder(itemView){
-        val receiveMessage=itemView.findViewById<TextView>(R.id.txt_receive_message)
-
+    inner class ReceivedViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val receivedMessage: TextView = itemView.findViewById(R.id.txt_receive_message)
     }
 
+    inner class GroupViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val groupSenderName: TextView = itemView.findViewById(R.id.txt_group_sender_name)
+        val groupMessage: TextView = itemView.findViewById(R.id.txt_group_message)
+    }
 }
