bf9ab3023ccaaa63a28b02307d978074d40e731f
Akshay@2003 <akshay88magar@gmail.com>
2023-06-09 00:15:46 +0530

    group layout and model class implemented

--- app/src/main/java/com/example/chatapplication/Message.kt
+++ app/src/main/java/com/example/chatapplication/Message.kt
@@ -1,12 +1,19 @@
 package com.example.chatapplication
 
 class Message {
-    var message : String?=null
-    var senderId:String?=null
+    var message: String? = null
+    var senderId: String? = null
+    var groupId: String? = null
+    var senderName: String? = null
 
-    constructor(){}
-    constructor(message: String?,senderId:String?){
-        this.message=message
-        this.senderId=senderId
+    constructor() {}
+
+    constructor(message: String?, senderId: String?, groupId: String?, senderName: String?) {
+        this.message = message
+        this.senderId = senderId
+        this.groupId = groupId
+        this.senderName = senderName
     }
+    // Getter and Setter methods
+
 }
